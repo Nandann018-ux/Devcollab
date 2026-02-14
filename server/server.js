58cee82b67a0a543b09808c9b66e3b1f742dfdfb
Nandan Acharya <samarthachar18@gmail.com>
2026-02-14 12:37:03 +0530

    Basic Backend and connected MongoDB

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`SERVER STARTED ON ${PORT}`);
+});
